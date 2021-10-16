d2eebda5f08d13e7cf00b5afba9f55355a235383
vatprcyq <vatprc_cyq@outlook.com>
2021-10-16 15:26:33 +0800

    Update task.sh

--- task.sh
+++ task.sh
@@ -1,30 +1,4 @@
 #!/bin/bash
-cd /root/dump1090
-./dump1090 --interactive --net
-ps -eaf | grep dump1090 | grep -v grep
-if [ $? -eq 1 ]
-then
-echo `date "+%G-%m-%d %H:%M:%S"`" dump1090            restart"
-echo "------------------------------------------------------------------------"
-systemctl restart dump1090-fa
-systemctl restart dump1090
-systemctl restart dump1090-mutability
-else
-echo `date "+%G-%m-%d %H:%M:%S"`" dump1090            running"
-echo "------------------------------------------------------------------------"
-fi
-
-sleep 10
-ps -eaf | grep dump1090 | grep -v grep
-if [ $? -eq 1 ]
-then
-echo `date "+%G-%m-%d %H:%M:%S"`" dump1090            restart"
-echo "------------------------------------------------------------------------"
-sleep 10
-else
-echo `date "+%G-%m-%d %H:%M:%S"`" dump1090            running"
-echo "------------------------------------------------------------------------"
-fi
 
 ps -eaf | grep send_message.py | grep -v grep
 # if not found - equals to 1, start it
